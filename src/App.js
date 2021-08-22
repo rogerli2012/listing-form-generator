d4bb5e2e4bf062b042cc74e88abc5df6396f9bf7
Roger Li <rogerli2012@gmail.com>
2021-08-22 00:04:33 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Listing Form Generator</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <form action="https://tinyurl.com/listing-form-demo">
+        <div className="input">
+          <input
+            id="input"
+            placeholder="Enter UPC code"
+            type="text"
+          />
+        </div>
+        <div className="select">
+          <select id="select">
+            <option>Select a retailer</option>
+            <option>UNFI</option>
+            <option>Amazon</option>
+            <option>Walmart</option>
+          </select>
+        </div>
+        <div>
+          <Button id="button" type="submit">Generate listing form</Button>
+        </div>
+      </form>
     </div>
   );
 }
